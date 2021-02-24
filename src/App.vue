75cdaa4246e52d19a254b411146b29c55ca21f64
sbasshub <sbasspro23@gmail.com>
2021-02-24 22:03:42 +0700

    final touch and styling the web

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <product-list-one></product-list-one>
-    <product-list-two></product-list-two>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
 export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
   name: 'app',
   data () {
     return {
@@ -22,5 +29,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,13 +2,17 @@
   <div id="product-list-two">
     <h2>List produk dua</h2>
     <ul>
-        <li v-for=""></li>
+        <li v-for="(product, index) in products" :key="index">
+          <span class="name"> {{ product.name }}, </span>
+          <span class="price">${{ product.price }} </span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['products'],
   data () {
     return {
       
@@ -17,6 +21,35 @@ export default {
 }
 </script>
 
-<style>
+<style scopeed>
+#product-list-two{
+    background: #D1E4FF;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-two ul{
+    padding: 0;
+    list-style-type: none;
+}
+#product-list-two li{
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
 
+#product-list-two li:hover{
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    color: #ffffff;
+    background: rgba(0,0,0,0.7);
+}
+
+.price{
+    font-weight: bold;
+    color: #ab77d4;
+    display: block;
+}
 </style>
